1c111419b079608cee328db1679420b8741be522
mudassir7732 <mudassir7732@gmail.com>
2024-05-12 15:33:58 +0500

    Code updates

--- Front-end/src/routes/index.js
+++ Front-end/src/routes/index.js
@@ -29,7 +29,6 @@ const AppRoutes = () => {
             if (userString !== null) {
                 user = JSON.parse(userString);
             }
-            console.log(user,' = UserJSON')
             setAdmin(user.admin)
         }
         catch (err) {
