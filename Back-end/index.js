33be0e0e0782d39d1dd124ce27fe717403b9d29d
mudassir7732 <mudassir7732@gmail.com>
2024-05-24 09:15:42 +0500

    Updated code

--- Back-end/index.js
+++ Back-end/index.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
-app.use('/uploads', express.static('uploads'));
 app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
