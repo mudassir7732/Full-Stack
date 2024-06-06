639073d8a835b8aa27cf6523c7e88b9f0c9a1287
mudassir7732 <mudassir7732@gmail.com>
2024-06-06 14:11:57 +0500

    Updated code

--- Front-end/src/pages/signin/styles.js
+++ Front-end/src/pages/signin/styles.js
@@ -1,7 +1,7 @@
 const styles = {
     container:'bg-white min-h-screen flex flex-row items-center justify-between w-full overflow-x-hidden',
-    formWrapper:'w-full h-screen flex items-center justify-center',
-    card:' border-2 flex flex-col items-center bg-white rounded-xl',    
+    formWrapper:'w-full h-screen flex flex-col items-center justify-center',
+    card:'flex flex-col items-center bg-white rounded-xl w-fit py-3 px-4',
     welcome:'text-[30px] font-bold mb-[8px] gradient-text font-sans',
     desc:'text-[#67748e] font-normal text-[16px] font-sans mb-[14px]',
     title:'text-[13px] text-[#344767] font-semibold ml-1 mt-[8px] font-sans mb-1',
@@ -9,7 +9,7 @@ const styles = {
     switchWrapper:'flex flex-row items-center justify-start mt-2',
     switch:'text-[#344767] font-normal text-[14px] ml-3 mt-2 font-sans',
     signin:'text-white gradient-button outline-none mt-3 w-[318px] shadow-md my-1 hover:bg-[#000060]v font-sans font-semibold text-[13px] rounded-[7px]',
-    singupWrapper:'flex flex-col sm:flex-row min-w-[50vw] w-fit items-center justify-center mt-3',
+    singupWrapper:'flex flex-col sm:flex-row w-fit items-center justify-center mt-3',
     signup:'ml-1 -mt-4 sm:mt-0 text-[13px] gradient-text font-medium cursor-pointer hover:text-[#63b3ed] font-sans',
     error:"text-red-500 text-[18px] font-semibold font-sans absolute bottom-12",
     bgImage:'h-screen w-[700px] -mr-[200px] transform -skew-x-12 rounded-bl-2xl',
